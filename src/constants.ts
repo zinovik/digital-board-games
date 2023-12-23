51d6a3dfa256bc399336bede5690ed044261cd57
Max Zinovik <zinovik@gmail.com>
2023-12-23 18:26:35 +0100

    update url

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const DIGITAL_BOARD_GAMES_URL =
 export const BGG_GAMES_RANKS_STATIC =
   'https://raw.githubusercontent.com/zinovik/bgg-games-ranks-data/main/bgg-games-ranks.json';
 export const BGG_GAMES_RANKS_FUNCTION =
-  'https://bgg-games-ranks-parser-wniawguk3a-uc.a.run.app?amount=2000';
+  'https://us-central1-zinovik-project.cloudfunctions.net/bgg-games-ranks-parser?amount=2000';
