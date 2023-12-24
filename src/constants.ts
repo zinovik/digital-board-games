f8dcdc172be0ff0ac1acd86a7ec2c3c955453f70
Max Zinovik <zinovik@gmail.com>
2023-12-24 10:29:51 +0100

    Revert url change

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const DIGITAL_BOARD_GAMES_URL =
   'https://raw.githubusercontent.com/zinovik/digital-board-games-data/main/digital-board-games.json';
 export const BGG_GAMES_RANKS_STATIC =
-  'https://storage.googleapis.com/boardgamegeek/top1000.json';
+  'https://raw.githubusercontent.com/zinovik/bgg-games-ranks-data/main/bgg-games-ranks.json';
